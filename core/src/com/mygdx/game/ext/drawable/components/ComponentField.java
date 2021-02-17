281b28451a27862998affaf10e6df508e0a60b86
Alice <77983090+fqqf@users.noreply.github.com>
2021-02-17 17:38:38 +0300

    added componentField to reuse components in diff actors

---
+++ core/src/com/mygdx/game/ext/drawable/components/ComponentField.java
@@ -0,0 +1,18 @@
+package com.mygdx.game.ext.drawable.components;
+
+import java.lang.invoke.ClassSpecializer;
+
+public class ComponentField2<T>
+{
+ T field;
+
+ public ComponentField2()
+ {
+  
+ }
+
+ public T get()
+ {
+  return
+ }
+}
