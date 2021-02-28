eb1d9c21a0799e151ae57c94ca8f4d6555f54cc1
Alice <77983090+fqqf@users.noreply.github.com>
2021-02-28 14:23:18 +0300

    Removed obsolete calls in Application loop

---
+++ core/src/com/mygdx/game/tests/test5/Main.java
@@ -0,0 +1,38 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.mygdx.game.ext.core.drawing.ApplicationLoop;
+import com.mygdx.game.ext.core.drawing.view.ExtendCoordinateGrid;
+import com.mygdx.game.ext.core.drawing.view.Monitor;
+
+public class Main extends ApplicationLoop
+{
+ City city;
+
+ public void create()
+ {
+  super.create();
+
+  new Monitor();
+
+  city = new City("city-level", new ExtendCoordinateGrid("level-coordinate-grid", 10f), 100, 10);
+ }
+
+ @Override
+ public void drawGraphics()
+ {
+  Gdx.gl.glClearColor(0.4f, 0.4f, 0.4f, 1);
+  Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+  city.iterDraw(extrapolation);
+ }
+
+ @Override
+ public void calcPhysics()
+ {
+  city.iterPhys();
+ }
+
+ public void resize(int width, int height) { Monitor.instance.update(width, height); }
+}
