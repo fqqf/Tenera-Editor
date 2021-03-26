7c2c801185a015099b09addc64cc9c93900f08b8
xWeb <29051616+eth5@users.noreply.github.com>
2021-03-26 11:45:29 +1000

    Refactoring(Component)
    removed imports

--- core/src/com/mygdx/game/ext/core/components/Component.java
+++ core/src/com/mygdx/game/ext/core/components/Component.java
@@ -1,10 +1,5 @@
 package com.mygdx.game.ext.core.components;
 
-import com.mygdx.game.ext.core.actor.Actor;
-import com.mygdx.game.ext.core.actor.interfaces.Func;
-
-import java.lang.invoke.MethodHandles;
-
 public abstract class Component
 {
 }
